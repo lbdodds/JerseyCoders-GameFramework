6e5a1d8fcc06e23717faa00e80e54e3206219eed
Liam Dodds <liam.b.dodds@gmail.com>
2015-04-18 14:59:55 +0100

    Create loggable interface

---
+++ src/me/liamdodds/framework/logging/Loggable.java
@@ -0,0 +1,8 @@
+package me.liamdodds.framework.logging;
+
+/**
+ * Created by Liam Cristoforo-Dodds on 18/04/2015.
+ */
+public interface Loggable {
+    void setLogger(Logger logger);
+}
--- src/me/liamdodds/framework/logging/Logger.java
+++ src/me/liamdodds/framework/logging/Logger.java
@@ -8,7 +8,7 @@ import java.awt.*;
  * Created by Liam Cristoforo-Dodds on 18/04/2015.
  */
 public interface Logger {
-    public void log(String cls, String message);
-    public void draw(Graphics2D g2d);
-    public void setFramework(Framework framework);
+    void log(String cls, String message);
+    void draw(Graphics2D g2d);
+    void setFramework(Framework framework);
 }
