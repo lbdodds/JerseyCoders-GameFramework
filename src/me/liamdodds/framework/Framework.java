c24389ff688be7426d87555ecabb091e9fa0082a
Liam Dodds <liam.b.dodds@gmail.com>
2015-04-18 14:28:02 +0100

    Create Logger Class

--- src/me/liamdodds/framework/Framework.java
+++ src/me/liamdodds/framework/Framework.java
@@ -1,9 +1,12 @@
 package me.liamdodds.framework;
 
+import me.liamdodds.framework.logging.Logger;
 import me.liamdodds.framework.screens.ScreenManager;
+import me.liamdodds.framework.screens.test.PrimaryScreen;
 import me.liamdodds.framework.utility.FPSLock;
 
 import java.awt.*;
+import java.util.Random;
 
 /**
  * The Game Framework that runs the game loop
@@ -11,7 +14,9 @@ import java.awt.*;
  */
 public class Framework extends Canvas {
 
+    private Random random;
     private ScreenManager screenManager;
+    private Logger logger;
 
     public Framework() {
         super();
@@ -33,7 +38,11 @@ public class Framework extends Canvas {
      * Initializes the framework
      */
     private void initialize() {
+        random = new Random();
         screenManager = new ScreenManager();
+        logger = new Logger(this);
+
+        screenManager.push("PrimaryScreen", new PrimaryScreen(Color.GRAY));
     }
 
     /**
@@ -64,6 +73,7 @@ public class Framework extends Canvas {
     @Override
     public void draw(Graphics2D g2d) {
         screenManager.draw(g2d);
+        logger.draw(g2d);
     }
 
     /**
@@ -73,4 +83,6 @@ public class Framework extends Canvas {
     public ScreenManager getScreenManager() {
         return screenManager;
     }
+
+    public Logger getLogger() { return logger; }
 }
