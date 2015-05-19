af6e476d2bec991b76c570393bd2807bd9304419
Liam Dodds <liam.b.dodds@gmail.com>
2015-05-19 23:22:29 +0100

    Added comments to SampleGame

--- src/me/lbcd/screens/BottleScreen.java
+++ src/me/lbcd/screens/BottleScreen.java
@@ -9,6 +9,7 @@ import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
 /**
+ * A sample screen that displays a bottle image that has been loaded
  * Created by Liam Cristoforo-Dodds on 19/05/15.
  */
 public class BottleScreen extends Screen {
@@ -40,12 +41,20 @@ public class BottleScreen extends Screen {
         return ScreenProcessState.PRIMARY;
     }
 
+    /**
+     * Draw that sweet sweet bottle
+     * @param g2d
+     */
     @Override
     public void draw(Graphics2D g2d) {
         BufferedImage bottles = gameData.getSpriteManager().get("bottles");
         g2d.drawImage(bottles, x, y, null);
     }
 
+    /**
+     * On update, check to see if you can move the bottle using the arrow keys
+     * @param gameData
+     */
     @Override
     public void update(GameData gameData) {
         super.update(gameData);
